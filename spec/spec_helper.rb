ac98a4b087a63f0d1f10aaefb330f010fb064211
Victor Sokolov <gzigzigzeo@gmail.com>
2012-01-09 15:53:41 +0400

    Global scopes initialization changed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,10 +4,6 @@ require 'rubygems'
 require 'bundler/setup'
 require 'wildcard_scopes'
 
-WildcardScopes::Railtie.config.wildcard_scopes = proc {
-  ActiveRecord::Base.wildcard_scope /ascend_by_(.*)/, proc { |a| order("#{table_name}.#{a.first} ASC") }
-}
-
 ActiveRecord::Base.establish_connection(
   "adapter" => "sqlite3", 
   "database" => ":memory:"
@@ -18,6 +14,8 @@ ActiveRecord::Schema.define :version => 0 do
     t.datetime :created_at
   end
 end
+
+ActiveRecord::Base.wildcard_scope /ascend_by_(.*)/, proc { |a| order("#{table_name}.#{a.first} ASC") }
 
 class User < ActiveRecord::Base
   wildcard_scope /descend_by_(.*)/, proc { |a| order("#{table_name}.#{a.first} DESC") }
